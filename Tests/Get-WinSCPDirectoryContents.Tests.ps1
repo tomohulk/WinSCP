9363cc1e2a0abd8d823fe8dc3220fe4655b5cdda
Thomas Malkewitz <Get-Alias@outlook.com>
2015-03-31 12:09:32 -0400

    Adding Pesters

---
+++ Tests/Get-WinSCPDirectoryContents.Tests.ps1
@@ -0,0 +1,45 @@
+Import-Module -Name "$((Get-Item -Path (Split-Path -Parent $MyInvocation.MyCommand.Path)).Parent)\WinSCP.psd1"
+
+Describe "Get-WinSCPDirectoryContents" {
+    It "WinSCP Module should be loaded." {
+        Get-Module -Name WinSCP | Should be $true
+    }
+
+    $params = @{
+        HostName = $env:COMPUTERNAME
+        UserName = "MyUser"
+        Password = "MyPassword"
+        Protocol = "Ftp"
+    }
+
+    Context "Get-WinSCPDirectoryContents -WinSCPSession `$session -Path `"./`"" {
+        $ftp = New-Item -Path "$(Get-Location)\Ftp" -ItemType Directory
+
+        It "Ftp directory should exist." {
+            Test-Path -Path $ftp | Should Be $true
+        }
+        
+        $session = Open-WinSCPSession -SessionOptions (New-WinSCPSessionOptions @params)
+
+        It "Session should be open." {
+            $session.Opened | Should Be $true
+        }
+
+        $result = Get-WinSCPDirectoryContents -WinSCPSession $session -Path "./"
+
+        It "object should be type WinSCP.RemoteDirectoryInfo."{
+            $result.GetType() | Should Be WinSCP.RemoteDirectoryInfo
+        }
+
+        It "Directory should only contain parent path." {
+            $result.Files | Should Be ".."
+        }
+
+        It "Session should be closed." {
+            Close-WinSCPSession -WinSCPSession $session
+            $session.Opened | Should Be $null
+        }
+
+        Remove-Item -Path $ftp -Confirm:$false -Force -Recurse
+    }
+}
