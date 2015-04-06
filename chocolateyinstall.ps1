9341870c77cfc77f8b065d7312559b85b72caf59
Thomas Malkewitz <Get-Alias@outlook.com>
2015-04-06 10:36:48 -0400

    Updated chocolateyinstall.ps1

--- chocolateyinstall.ps1
+++ chocolateyinstall.ps1
@@ -1,15 +1,169 @@
-﻿$sourceFiles = Get-Item -Path ".\lib\winscp.powershell"
-$moduleName = (Get-ChildItem -Path $sourceFiles -Filter "*.psd1").ToString().TrimEnd(".psd1")
-$userModuleDirectory = "$($env:USERPROFILE)\Documents\WindowsPowerShell\Modules"
-$moduleDirectory = Join-Path -Path $userModuleDirectory -ChildPath $moduleName
+﻿[CmdletBinding()]
+Param ( )
 
-if (Test-Path $moduleDirectory)
+Begin
 {
-    Remove-Item -Path $moduleDirectory -Recurse -Force -Confirm:$false
+    Function Update-Directory
+    {
+        [CmdletBinding()]
+        param (
+            [Parameter(Mandatory = $true)]
+            [String] 
+            $Source,
+
+            [Parameter(Mandatory = $true)]
+            [String] 
+            $Destination
+        )
+
+        $Source = $PSCmdlet.GetUnresolvedProviderPathFromPSPath($Source)
+        $Destination = $PSCmdlet.GetUnresolvedProviderPathFromPSPath($Destination)
+
+        if (-not (Test-Path -LiteralPath $Destination))
+        {
+            $null = New-Item -Path $Destination -ItemType Directory -ErrorAction Stop
+        }
+
+        try
+        {
+            $sourceItem = Get-Item -LiteralPath $Source -ErrorAction Stop
+            $destItem = Get-Item -LiteralPath $Destination -ErrorAction Stop
+
+            if ($sourceItem -isnot [System.IO.DirectoryInfo] -or $destItem -isnot [System.IO.DirectoryInfo])
+            {
+                throw 'Not Directory Info'
+            }
+        }
+        catch
+        {
+            throw 'Both Source and Destination must be directory paths.'
+        }
+
+        $sourceFiles = Get-ChildItem -Path $Source -Recurse | ?{ -not $_.PSIsContainer }
+
+        foreach ($sourceFile in $sourceFiles)
+        {
+            $relativePath = Get-RelativePath $sourceFile.FullName -RelativeTo $Source
+            $targetPath = Join-Path $Destination $relativePath
+
+            $sourceHash = Get-FileHash -Path $sourceFile.FullName
+            $destHash = Get-FileHash -Path $targetPath
+
+            if ($sourceHash -ne $destHash)
+            {
+                $targetParent = Split-Path $targetPath -Parent
+
+                if (-not (Test-Path -Path $targetParent -PathType Container))
+                {
+                    $null = New-Item -Path $targetParent -ItemType Directory -ErrorAction Stop
+                }
+
+                Write-Verbose "Updating file $relativePath to new version."
+                Copy-Item $sourceFile.FullName -Destination $targetPath -Force -ErrorAction Stop
+            }
+        }
+
+        $targetFiles = Get-ChildItem -Path $Destination -Recurse | ?{ -not $_.PSIsContainer }
+    
+        foreach ($targetFile in $targetFiles)
+        {
+            $relativePath = Get-RelativePath $targetFile.FullName -RelativeTo $Destination
+            $sourcePath = Join-Path $Source $relativePath        
+
+            if (-not (Test-Path $sourcePath -PathType Leaf))
+            {
+                Write-Verbose "Removing unknown file $relativePath from module folder."
+                Remove-Item -LiteralPath $targetFile.FullName -Force -ErrorAction Stop
+            }
+        }
+
+    }
+
+    Function Get-RelativePath
+    {
+        Param 
+        ( 
+            [Parameter()]
+            [String] 
+            $Path, 
+            
+            [Parameter()]
+            [String] 
+            $RelativeTo 
+        )
+
+        return $Path -replace "^$([regex]::Escape($RelativeTo))\\?"
+    }
+
+    Function Get-FileHash
+    {
+        Param 
+        
+            ([String] 
+            $Path
+        )
+
+        if (-not (Test-Path -LiteralPath $Path -PathType Leaf))
+        {
+            return $null
+        }
+
+        $item = Get-Item -LiteralPath $Path
+        if ($item -isnot [System.IO.FileSystemInfo])
+        {
+            return $null
+        }
+
+        $stream = $null
+
+        try
+        {
+            $sha = New-Object System.Security.Cryptography.SHA256CryptoServiceProvider
+            $stream = $item.OpenRead()
+            $bytes = $sha.ComputeHash($stream)
+            return [convert]::ToBase64String($bytes)
+        }
+        finally
+        {
+            if ($null -ne $stream) 
+            { 
+                $stream.Close()
+            }
+
+            if ($null -ne $sha)
+            { 
+                $sha.Clear()
+            }
+        }
+    }
 }
 
-Copy-Item -Path $sourceFiles -Destination $moduleDirectory -Recurse
+End
+{
+    $modulePath = Join-Path $env:ProgramFiles WindowsPowerShell\Modules
+    $targetDirectory = Join-Path $modulePath WinSCP
 
-Get-ChildItem -Path $moduleDirectory -Include "chocolateyinstall.ps1","chocolateyuninstall.ps1","*.nupkg" -Recurse | %{ Remove-Item -Path $_.FullName -Force -Confirm:$false }
+    $scriptRoot = Split-Path $MyInvocation.MyCommand.Path -Parent
+    $sourceDirectory = Join-Path $scriptRoot Tools
 
-Import-Module -Name (Get-Item -Path $moduleDirectory)
+    Update-Directory -Source $sourceDirectory -Destination $targetDirectory
+
+    if ($PSVersionTable.PSVersion.Major -lt 4)
+    {
+        $modulePaths = [Environment]::GetEnvironmentVariable('PSModulePath', 'Machine') -split ';'
+        if ($modulePaths -notcontains $modulePath)
+        {
+            Write-Verbose "Adding '$modulePath' to PSModulePath."
+
+            $modulePaths = @(
+                $modulePath
+                $modulePaths
+            )
+
+            $newModulePath = $modulePaths -join ';'
+
+            [Environment]::SetEnvironmentVariable('PSModulePath', $newModulePath, 'Machine')
+            $env:PSModulePath += ";$modulePath"
+        }
+    }
+}
