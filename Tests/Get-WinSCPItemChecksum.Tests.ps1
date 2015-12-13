355ee84e281fb9d8e397b15dd1ea03d6236a9a89
Thomas Malkewitz <get-alias@outlook.com>
2015-12-13 15:07:58 -0500

    Update Pesters

--- Tests/Get-WinSCPItemChecksum.Tests.ps1
+++ Tests/Get-WinSCPItemChecksum.Tests.ps1
@@ -1,13 +1,13 @@
 #requires -Modules Pester,PSScriptAnalyzer
 
 Set-Location -Path "$env:USERPROFILE\Documents\GitHub\WinSCP"
-Import-Module -Name .\WinSCP.psd1 -Force
+Import-Module -Name .\WinSCP\WinSCP.psd1 -Force
 
 Get-Process | Where-Object { $_.Name -eq 'WinSCP' } | Stop-Process -Force
 
 Describe 'Get-WinSCPItemChecksum' {
     Context "Invoke-ScriptAnalyzer -Path $(Resolve-Path -Path (Get-Location))\Functions\Get-WinSCPItemChecksum.ps1." {
-        $results = Invoke-ScriptAnalyzer -Path .\Functions\Get-WinSCPItemChecksum.ps1
+        $results = Invoke-ScriptAnalyzer -Path .\WinSCP\Public\Get-WinSCPItemChecksum.ps1
 
         It 'Invoke-ScriptAnalyzer of Get-WinSCPItemChecksum results count should be 0.' {
             $results.Count | Should Be 0
