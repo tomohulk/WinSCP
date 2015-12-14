20e98a7adcccad38e12a42bcec5505cb53983174
Thomas Malkewitz <get-alias@outlook.com>
2015-12-14 16:08:27 -0500

    Appveyor work.

--- Tests/appveyor.pester.ps1
+++ Tests/appveyor.pester.ps1
@@ -3,10 +3,18 @@ Set-Location -Path $ENV:APPVEYOR_BUILD_FOLDER
 
 $timestamp = Get-Date -uformat "%Y%m%d-%H%M%S"
 $psVersion = $PSVersionTable.PSVersion.Major
-$results = "Results_PS${psVersion}_${timestamp}.xml"
+$resultsFile = "Results_PS${psVersion}_${timestamp}.xml"
 
 Import-Module -Name .\WinSCP\WinSCP.psd1 -Force
 
 Invoke-Pester -Path '.\Tests' -OutputFormat NUnitXml -OutputFile ".\$results"
 
-(New-Object -TypeName System.Net.WebClient).UploadFile("https://ci.appveyor.com/api/testresults/nunit/$($env:APPVEYOR_JOB_ID)", ".\$results")
+(New-Object -TypeName System.Net.WebClient).UploadFile("https://ci.appveyor.com/api/testresults/nunit/$($env:APPVEYOR_JOB_ID)", ".\$resultsFile")
+
+$results = @( Get-ChildItem -Path "$ProjectRoot\PesterResults_PS*.xml" | Import-Clixml )
+            
+$failedCount = 
+    
+if (($results | Select-Object -ExpandProperty FailedCount |Measure-Object -Sum |Select-Object -ExpandProperty Sum) -gt 0) {
+    throw "Build failed."
+}
