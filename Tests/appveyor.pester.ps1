2091c245952ed072ee8c02c7e4463ff85cf8500d
Thomas Malkewitz <get-alias@outlook.com>
2015-12-15 08:45:21 -0500

    corrections to appveyor.pester.ps1

--- Tests/appveyor.pester.ps1
+++ Tests/appveyor.pester.ps1
@@ -1,16 +1,18 @@
 #requires -Modules Pester
 try {
-    Set-Location -Path $ENV:APPVEYOR_BUILD_FOLDER -ErrorAction Stop
+    Set-Location -Path $env:APPVEYOR_BUILD_FOLDER -ErrorAction Stop
 
     $timestamp = Get-Date -uformat "%Y%m%d-%H%M%S"
-    $psVersion = $PSVersionTable.PSVersion.Major
-    $resultsFile = "Results_PS${psVersion}_${timestamp}.xml"
+    $resultsFile = "Results_$timestamp.xml"
 
+    Import-Module -Name Pester -Force -ErrorAction Stop
+    Import-Module -Name PSScriptAnalyzer -Force -ErrorAction Stop
     Import-Module -Name .\WinSCP\WinSCP.psd1 -Force -ErrorAction Stop
 
-    Invoke-Pester -Path '.\Tests' -OutputFormat NUnitXml -OutputFile ".\$resultsFile" -ErrorAction Stop
+    Invoke-Pester -Path '.\Tests' -OutputFormat NUnitXml -OutputFile ".\Tests" -PassThru -ErrorAction Stop | 
+        Export-Clixml -Path .\$resultsFile
 
-    (New-Object -TypeName System.Net.WebClient -ErrorAction Stop).UploadFile("https://ci.appveyor.com/api/testresults/nunit/$($env:APPVEYOR_JOB_ID)", ".\$resultsFile")
+    (New-Object -TypeName System.Net.WebClient).UploadFile("https://ci.appveyor.com/api/testresults/nunit/$($env:APPVEYOR_JOB_ID)", ".\$resultsFile")
     
     if ((Import-Clixml -Path .\$resultsFile -ErrorAction Stop | Select-Object -ExpandProperty FailedCount | Measure-Object -Sum | Select-Object -ExpandProperty Sum) -gt 0) {
         throw "Build failed."
