666fbedaa62e41f8ac010a34ce1cfab6e3ec6fe4
Thomas Malkewitz <get-alias@outlook.com>
2015-12-15 09:26:55 -0500

    la de da de da.

--- Tests/appveyor.pester.ps1
+++ Tests/appveyor.pester.ps1
@@ -3,7 +3,7 @@ try {
     Set-Location -Path $env:APPVEYOR_BUILD_FOLDER -ErrorAction Stop
 
     $timestamp = Get-Date -uformat "%Y%m%d-%H%M%S"
-    $resultsFile = "Results_$timestamp.xml"
+    $resultsFile = "Results_${timestamp}.xml"
 
     Import-Module -Name Pester -Force -ErrorAction Stop
     Import-Module -Name PSScriptAnalyzer -Force -ErrorAction Stop
@@ -12,7 +12,7 @@ try {
     Invoke-Pester -Path '.\Tests' -OutputFormat NUnitXml -OutputFile ".\$resultsFile" -PassThru -ErrorAction Stop | 
         Export-Clixml -Path ".\Pester$resultsFile"
 
-    (New-Object -TypeName System.Net.WebClient).UploadFile("https://ci.appveyor.com/api/testresults/nunit/$($env:APPVEYOR_JOB_ID)", ".\$resultsFile")
+    (New-Object -TypeName System.Net.WebClient).UploadFile("https://ci.appveyor.com/api/testresults/nunit/$($env:APPVEYOR_JOB_ID)", (Resolve-Path -Path ".\$resultsFile"))
     
     if ((Import-Clixml -Path ".\Pester$resultsFile" -ErrorAction Stop | Select-Object -ExpandProperty FailedCount | Measure-Object -Sum | Select-Object -ExpandProperty Sum) -gt 0) {
         throw "Build failed."
