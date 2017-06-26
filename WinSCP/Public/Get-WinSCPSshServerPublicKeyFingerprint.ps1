fbc75c3eb4a12a61f671903500931f1821137bc6
Thomas Malkewitz <dotps1@outlook.com>
2017-06-26 13:00:13 -0400

    Add Get-WinSCPSshServerPublicKeyFingerprint function, #56.

---
+++ WinSCP/Public/Get-WinSCPSshServerPublicKeyFingerprint.ps1
@@ -0,0 +1,43 @@
+function Get-WinSCPSshServerPublicKeyFingerprint {
+
+    [CmdletBinding(
+        HelpUri = "https://dotps1.github.io/WinSCP/Get-WinSCPSshServerPublicKeyFingerprint.html"
+    )]
+    [OutputType(
+        [String]
+    )]
+
+    param (
+        [Parameter(
+            Mandatory = $true,
+            ValueFromPipeline = $true,
+            ValueFromPipelineByPropertyName = $true
+        )]
+        [WinSCP.SessionOptions[]]
+        $SessionOption
+    )
+
+    begin {
+        $session = New-Object -TypeName WinSCP.Session
+    }
+
+    process {
+        foreach ($sessionOptionValue in $SessionOption) {
+            try {
+                $output = $session.ScanFingerprint(
+                    $sessionOptionValue
+                )
+
+                Write-Output -InputObject $output
+            } catch {
+                $PSCmdlet.WriteError(
+                    $_
+                )
+            }
+        }
+    }
+
+    end {
+        $session.Dispose()
+    }
+}
