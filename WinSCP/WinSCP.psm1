56905a5e36783ba2b367ce5360f265f4a2d379b9
Thomas Malkewitz <thomas.malkewitz@icloud.com>
2020-08-20 10:33:26 -0400

    Add logic to psm1 to load the proper .net assembly for Desktop or Core PowerShell versions.  Issue #130.

--- WinSCP/WinSCP.psm1
+++ WinSCP/WinSCP.psm1
@@ -1,19 +1,52 @@
+$moduleRoot = Split-Path -Path $MyInvocation.MyCommand.Path
+
+
+#region LoadAssemblies
+
+switch ($PSVersionTable.PSEdition) {
+    "Core" {
+        Add-Type -Path "${moduleRoot}\lib\netstandard2.0\WinSCPnet.dll"
+        break;
+    }
+
+    "Desktop" {
+        Add-Type -Path "${moduleRoot}\lib\net40\WinSCPnet.dll"
+        break;
+    }
+
+    default {
+        Write-Error -Message "Failed to find a compatiable WinSCP Assembly."
+        exit
+    }
+}
+
+#endregion LoadAssemblies
+
+
+#region LoadFunctions
+
 $paths = @(
     "Private",
     "Public"
 )
 
 foreach ($path in $paths) {
-    "$(Split-Path -Path $MyInvocation.MyCommand.Path)\$path\*.ps1" | 
+    "${moduleRoot}\${path}\*.ps1" | 
         Resolve-Path | 
             ForEach-Object { 
 	            . $_.ProviderPath 
             }
 }
 
-# Add aliases.
+#endregion LoadFunctions
+
+
+#region Aliases
+
 New-Alias -Name "Enter-WinSCPSession" -Value "New-WinSCPSession"
 New-Alias -Name "Open-WinSCPSession" -Value "New-WinSCPSession"
 
 New-ALias -Name "Close-WinSCPSession" -Value "Remove-WinSCPSession"
 New-Alias -Name "Exit-WinSCPSession" -Value "Remove-WinSCPSession"
+
+#endregion Aliases
